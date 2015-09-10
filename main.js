fb3f96a467b35f6e85d2b5611908ec98504a31df
Student <student@RU-017.local>
2015-09-10 17:08:22 -0600

    incomplete end of day

--- main.js
+++ main.js
@@ -1,30 +1,63 @@
-angular.module('myApp', [])
+// P A R T  1 //
 
-var mainControllerFunc = function($scope){
-	$scope.title = " First Angular Page";
-	$scope.myHeader = false
-	$scope.myText = false
 
+// angular.module('myApp', [])
+
+// var mainControllerFunc = function($scope){
+// 	$scope.title = " First Angular Page";
+// 	$scope.myHeader = false
+// 	$scope.myText = false
+// 	$myList = false
 
 	
 
-$scope.buttonClick = function(){
+// $scope.buttonClick = function(){
 
-	// console.log("you clicked a button")
+// 	// console.log("you clicked a button")
 
-	$scope.myHeader = !$scope.myHeader
-	$scope.myText = !$scope.myText
+// 	$scope.myHeader = !$scope.myHeader
+// 	$scope.myText = !$scope.myText
 
 
 
-	}
+// 	}
 
+// $scope.click2 = function(){
+
+// 	$scope.myList = !$scope.myList
+	
+// 	}
+
+// }
+
+
+// angular.module('myApp')
+// .controller('mainController' , ['$scope', mainControllerFunc])
+
+// ---------------------------------------------------------
+
+// P A R T  2 //
+
+angular.module('myApp', [])
+
+var mainControllerFunc = function($scope){
+
+$scope.mouseenter = function(){
+
+	$scope.hovering = true;
+
+}
+
+$scope.mouseleave = function(){
+
+
+	$scope.hovering = false;
+	console.log(event)
 }
 
 
 
-
-
+}
 
 angular.module('myApp')
 .controller('mainController' , ['$scope', mainControllerFunc])
